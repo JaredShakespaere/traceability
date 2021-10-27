985c62d43a80211f050c5a2fdbb7c3092e15709f
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-10-26 22:27:55 -0500

    commit

--- server.js
+++ server.js
@@ -12,5 +12,11 @@ app.use(express.json())
 
 const port = process.env.PORT || 4400
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 
-app.listen(port, () => console.log(`Take us to warp ${port}`))
+})
+
+const port = process.env.PORT || 4400
+
+app.listen(port, () => console.log(`Running on port ${port}`))
