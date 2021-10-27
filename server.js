b198b3505045db5c71b2969f5762abbecd6456bb
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-10-27 00:08:58 -0500

    rollbar

--- server.js
+++ server.js
@@ -19,13 +19,17 @@ app.get('/', (req, res) => {
     rollbar.info('html file served successfully')
 })
 
-app.post('/test', (req, res) => {
-    try {
-        nonExistentFunction();
-    } catch (error) {
-        console.error(error);
-    }
+app.post('/api/student', (req, res) => {
+    const {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+    rollbar.log("Student added successfully", {author: 'Jared', type: 'manual entry'})
+    res.status(200).send(students)
+
 })
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4400
 
