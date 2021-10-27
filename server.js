104478e2f89c969054521a76a459590bd3cf06fe
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-10-26 22:34:27 -0500

    alter express json

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const path = require('path')
 
 
 const app = express()
-app.use(express.json())
+// app.use(express.json())
 
-// const Rollbar = require('rollbar')
 
-// let rollbar = new Rollbar({})
-
-const port = process.env.PORT || 4400
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
