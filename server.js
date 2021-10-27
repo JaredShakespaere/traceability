fb601ee2c7189f20c7e8a782a629faca913991ec
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-10-26 23:01:00 -0500

    added rollbar

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require('express')
 
 const path = require('path')
 
+const Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+    accessToken: 'b2a88862bcb34f52a1e6f17a5ddd950d',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express()
 // app.use(express.json())
 
 
 
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
-
+    rollbar.info('html file served successfully')
 })
 
 const port = process.env.PORT || 4400
